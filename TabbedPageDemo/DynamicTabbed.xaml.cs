8e4d32412bd68287381fe1f7a6ab0122d9570356
Home <Home@LAPTOP-2E6V6ACM>
2021-03-02 15:35:30 +0200

    Add project files.

---
+++ TabbedPageDemo/DynamicTabbed.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPageDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DynamicTabbed : Xamarin.Forms.TabbedPage
+    {
+        public DynamicTabbed()
+        {
+            InitializeComponent();
+            CreateTabs();
+        }
+        private void CreateTabs()
+        {
+            On<Windows>().SetHeaderIconsEnabled(true);
+            On<Windows>().SetHeaderIconsSize(new Size(48, 48));
+
+
+            Xamarin.Forms.Page tab1 = new Tab1();
+            tab1.Title = "Tab 1";
+            tab1.IconImageSource = "schedule.png";
+            Xamarin.Forms.Page tab2 = new Tab2();
+            tab2.Title = "Tab 2";
+            tab2.IconImageSource = "settings.png";
+
+            this.Children.Add(tab1);
+            this.Children.Add(tab2);
+
+        }
+    }
+}
